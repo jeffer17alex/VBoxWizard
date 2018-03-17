25e8096e7be45d78e8bdc15386ec448c8e2b9399
invitado <invitado@s3pc3.eisc.univalle.edu.co>
2018-03-17 11:03:44 -0500

    wizard

--- wizard.sh
+++ wizard.sh
@@ -1,19 +1,42 @@
 #!/bin/bash
 
 while [ 1 ]; do
-dialog --inputbox "Nombre de la maquina: " 8 40 2>nombre
-varnombre=$(cat nombre)
-VBoxManage list vms | grep ${varnombre}
-if [ $? -eq 1 ]; then
-break
-fi
+  #dialog --inputbox "Nombre de la maquina: " 8 40 2>nombre
+  varnombre=$(dialog --inputbox "Nombre de la maquina: " 8 40 3>&1 1>&2 2>&3 3>&-)
+  #varnombre=$(cat nombre)
+  VBoxManage list vms | grep -w ${varnombre}
+  if [ $? -eq 1 ]; then
+    break
+  fi
 done
 
-
-
+PROC=$(cat /proc/cpuinfo | grep processor | wc -l)
 NCORE="dialog --backtitle \"# CPU\" --radiolist \"Select CPU \" 10 40 4"
+
 for i in $(seq 1 1 2); do
-NCORE="${NCORE} ${i} ${i} off"
+  NCORE="${NCORE} ${i} ${i} off"
 done
+
 NCORE="${NCORE} 2> salida"
-Eval $NCORE
+eval $NCORE
+
+#DISK
+
+HDSIZE=$(dialog --backtitle "Disco duro " --radiolist "Capacidad " 0 0 0 1 512 on 2 2048 off 3 4096 off 4 8192)
+
+case ${HDSIZE} in
+  1)
+    HDSIZE=512
+    ;;
+  2)
+    HDSIZE=2048
+    ;;
+  3)
+    HDSIZE=4096
+    ;;
+  4)
+    HDSIZE=8192
+    ;;
+esac
+
+echo $HDSIZE
